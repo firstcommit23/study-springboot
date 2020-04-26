167c8251dec7183fd10a7265db7f148125af759d
jm131313 <jm131313.kim@gmail.com>
2020-04-26 22:36:11 +0900

    머스테치로 화면 구성하기 (.ignore 확인용)

--- src/main/java/com/jemizem/book/springboot/service/PostsService.java
+++ src/main/java/com/jemizem/book/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.jemizem.book.springboot.service;
 
 import com.jemizem.book.springboot.domain.posts.Posts;
 import com.jemizem.book.springboot.domain.posts.PostsRepository;
+import com.jemizem.book.springboot.web.dto.PostsListResponseDto;
 import com.jemizem.book.springboot.web.dto.PostsResponseDto;
 import com.jemizem.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jemizem.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +37,21 @@ public class PostsService {
                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
        return new PostsResponseDto(entity);
+    }
 
+    @Transactional //(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
